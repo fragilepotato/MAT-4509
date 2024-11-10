608297810d595638c7ea0a3760d9d64d3eb22b87
Md. Mahdi Kamal Alif <mkalif9@gmail.com>
2024-11-11 02:40:12 +0600

    loading the files and viewing them for the first time

--- test.r
+++ test.r
@@ -1,34 +1,15 @@
+# Install and load readxl package
+install.packages("readxl")
 library(readxl)
-library(fdth)
 
-####################################
-########## Data Loading ############
-####################################
-data <- read_excel("B:/BSMRAAU 22024010 5th/MAT_4509/Programming/United Airlines Aircraft Operating Statistics- Cost Per Block Hour (Unadjusted).xls", range="b2:w158")
-data <- as.data.frame(data)
+# Load the Excel file
+data <- read_excel("D:/vscode/R/United Airlines Aircraft Operating Statistics- Cost Per Block Hour (Unadjusted).xls")
 
-fleets <- c("Small Narrowbodies", "Large Narrowbodies", "Widebodies", "Total Fleet")
-wages_rows <- c(8, 47, 86, 125) - 2
+# View the first few rows
+head(data)
 
-####################################
-########## Functions ###############
-####################################
-get_row <- function(row, data) {
-  na.omit(as.numeric(data[row, -1]))[1:10]
-}
+# View the data in RStudio's data viewer
+View(data)
 
-# Function to create and print frequency distribution tables
-create_table <- function(data, fleet_name) {
-  interval <- as.integer((max(data) - min(data)) / (log2(length(data)) + 1))
-  table <- fdt(data, start = min(data), end = max(data), h = interval)
-  print(paste("Frequency Distribution Table for", fleet_name))
-  print(as.data.frame(table$table))  # Convert to data frame and print for readability
-}
-
-####################################
-############ Main code #############
-####################################
-for (i in seq_along(wages_rows)) {
-  pilots_wages <- get_row(wages_rows[i], data)
-  create_table(pilots_wages, fleets[i])
-}
+# Check the structure of the data
+str(data)
